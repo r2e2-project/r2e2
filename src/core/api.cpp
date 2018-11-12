7f07d4a515a14ab72cc5c178d512a687c5f89bd8
Sadjad Fouladi <sfouladi@gmail.com>
2018-11-12 13:45:42 -0800

    api.cpp: Add 'cloud' option to MakeIntegrator.

--- src/core/api.cpp
+++ src/core/api.cpp
@@ -61,6 +61,7 @@
 #include "integrators/sppm.h"
 #include "integrators/volpath.h"
 #include "integrators/whitted.h"
+#include "cloud/integrator.h"
 #include "lights/diffuse.h"
 #include "lights/distant.h"
 #include "lights/goniometric.h"
@@ -1706,6 +1707,8 @@ Integrator *RenderOptions::MakeIntegrator() const {
         integrator = CreateAOIntegrator(IntegratorParams, sampler, camera);
     } else if (IntegratorName == "sppm") {
         integrator = CreateSPPMIntegrator(IntegratorParams, camera);
+    } else if (IntegratorName == "cloud") {
+        integrator = CreateCloudIntegrator(IntegratorParams, sampler, camera);
     } else {
         Error("Integrator \"%s\" unknown.", IntegratorName.c_str());
         return nullptr;
