4d5aa6cf1ec275b64381e3109301abaaacc65f59
Sadjad Fouladi <sfouladi@gmail.com>
2018-12-02 14:11:02 -0800

    Add to/from_protobuf from TriangleMesh.

--- src/messages/utils.cpp
+++ src/messages/utils.cpp
@@ -1,6 +1,15 @@
 #include "utils.h"
 
+#include <stdexcept>
+
 namespace pbrt {
+
+protobuf::Point2f to_protobuf(const Point2f & point) {
+    protobuf::Point2f proto_point;
+    proto_point.set_x(point.x);
+    proto_point.set_y(point.y);
+    return proto_point;
+}
 
 protobuf::Point3f to_protobuf(const Point3f & point) {
     protobuf::Point3f proto_point;
@@ -36,6 +45,28 @@ protobuf::AnimatedTransform to_protobuf(const AnimatedTransform & transform) {
     return proto_transform;
 }
 
+protobuf::TriangleMesh to_protobuf(const TriangleMesh& tm) {
+    protobuf::TriangleMesh proto_tm;
+    proto_tm.set_n_triangles(tm.nTriangles);
+    proto_tm.set_n_vertices(tm.nVertices);
+
+    for (size_t i = 0; i < tm.nTriangles; i++) {
+        proto_tm.add_vertex_indices(tm.vertexIndices[3 * i]);
+        proto_tm.add_vertex_indices(tm.vertexIndices[3 * i + 1]);
+        proto_tm.add_vertex_indices(tm.vertexIndices[3 * i + 2]);
+    }
+
+    for (size_t i = 0; i < tm.nVertices; i++) {
+        *proto_tm.add_p() = to_protobuf(tm.p[i]);
+    }
+
+    if (tm.uv != nullptr || tm.n != nullptr || tm.s != nullptr) {
+        throw std::runtime_error("TriangleMesh: uv, n and s are not supported");
+    }
+
+    return proto_tm;
+}
+
 Point3f from_protobuf(const protobuf::Point3f & point) {
     return {point.x(), point.y(), point.z()};
 }
@@ -52,6 +83,28 @@ Matrix4x4 from_protobuf(const protobuf::Matrix & proto_matrix) {
         }
     }
     return matrix;
+}
+
+TriangleMesh from_protobuf(const protobuf::TriangleMesh &proto_tm) {
+    Transform identity;
+    std::vector<int> vertexIndices;
+    std::vector<Point3f> p;
+
+    vertexIndices.reserve(proto_tm.n_triangles() * 3);
+    p.reserve(proto_tm.n_vertices());
+
+    for (size_t i = 0; i < proto_tm.n_triangles(); i++) {
+        vertexIndices.push_back(proto_tm.vertex_indices(3 * i));
+        vertexIndices.push_back(proto_tm.vertex_indices(3 * i + 1));
+        vertexIndices.push_back(proto_tm.vertex_indices(3 * i + 2));
+    }
+
+    for (size_t i = 0; i < proto_tm.n_vertices(); i++) {
+        p.push_back(from_protobuf(proto_tm.p(i)));
+    }
+
+    return {identity, proto_tm.n_triangles(), vertexIndices.data(), proto_tm.n_vertices(),
+            p.data(), nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
 }
 
 }
--- src/messages/utils.h
+++ src/messages/utils.h
@@ -2,20 +2,23 @@
 #define PBRT_MESSAGES_UTILS_H
 
 #include "geometry.h"
-#include "transform.h"
 #include "pbrt.pb.h"
+#include "shapes/triangle.h"
+#include "transform.h"
 
 namespace pbrt {
 
-    protobuf::Point3f to_protobuf(const Point3f & point);
-    protobuf::Bounds3f to_protobuf(const Bounds3f & bounds);
-    protobuf::Matrix to_protobuf(const Matrix4x4 & matrix);
-    protobuf::AnimatedTransform to_protobuf(const AnimatedTransform & transform);
+protobuf::Point3f to_protobuf(const Point3f& point);
+protobuf::Bounds3f to_protobuf(const Bounds3f& bounds);
+protobuf::Matrix to_protobuf(const Matrix4x4& matrix);
+protobuf::AnimatedTransform to_protobuf(const AnimatedTransform& transform);
+protobuf::TriangleMesh to_protobuf(const TriangleMesh& triangleMesh);
 
-    Point3f from_protobuf(const protobuf::Point3f & point);
-    Bounds3f from_protobuf(const protobuf::Bounds3f & bounds);
-    Matrix4x4 from_protobuf(const protobuf::Matrix & matrix);
+Point3f from_protobuf(const protobuf::Point3f& point);
+Bounds3f from_protobuf(const protobuf::Bounds3f& bounds);
+Matrix4x4 from_protobuf(const protobuf::Matrix& matrix);
+TriangleMesh from_protobuf(const protobuf::TriangleMesh& mesh);
 
-}
+}  // namespace pbrt
 
 #endif /* PBRT_MESSAGES_UTILS_H */
