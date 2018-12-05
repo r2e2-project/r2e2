3b19c13c64a5183a18f72583b0aa36aff911c1b1
Sadjad Fouladi <sfouladi@gmail.com>
2018-12-04 22:45:05 -0800

    pbrt.cpp: Add --dumpscene option.

--- src/core/pbrt.h
+++ src/core/pbrt.h
@@ -179,6 +179,7 @@ struct Options {
     bool quickRender = false;
     bool quiet = false;
     bool cat = false, toPly = false;
+    std::string dumpScenePath{};
     std::string imageFile;
     // x0, x1, y0, y1
     Float cropWindow[2][2];
--- src/main/pbrt.cpp
+++ src/main/pbrt.cpp
@@ -68,6 +68,7 @@ Reformatting options:
   --toply              Print a reformatted version of the input file(s) to
                        standard output and convert all triangle meshes to
                        PLY files. Does not render an image.
+  --dumpscene <dir>    Dump scene data to <dir>
 )");
     exit(msg ? 1 : 0);
 }
@@ -130,7 +131,16 @@ int main(int argc, char *argv[]) {
         }
         else if (!strcmp(argv[i], "--logtostderr")) {
           FLAGS_logtostderr = true;
-        } else if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-help") ||
+        }
+        else if (!strcmp(argv[i], "--dumpscene") ||!strcmp(argv[i], "-dumpscene")) {
+            if (i + 1 == argc)
+                usage("missing value after --dumpscene argument");
+            options.dumpScenePath = argv[++i];
+        }
+        else if (!strncmp(argv[i], "--dumpscene=", 12)) {
+            options.dumpScenePath = &argv[i][12];
+        }
+        else if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-help") ||
                    !strcmp(argv[i], "-h")) {
             usage();
             return 0;
