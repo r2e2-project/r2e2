356322f9f579961f9465500da905f8a2fd11eeb1
Sadjad Fouladi <sfouladi@gmail.com>
2018-12-11 13:30:42 -0800

    lambda-worker.cpp: Make connection back to the coordinator.

--- src/cloud/lambda-worker.cpp
+++ src/cloud/lambda-worker.cpp
@@ -1,1 +1,57 @@
-int main() {}
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+#include "execution/loop.hh"
+#include "execution/meow/message.hh"
+#include "net/address.hh"
+#include "util/exception.h"
+
+using namespace std;
+
+class ProgramFinished : public exception {};
+
+void usage(const char* argv0) {
+    cerr << "Usage: " << argv0 << " DESTINATION PORT" << endl;
+}
+
+int main(int argc, char const* argv[]) {
+    try {
+        if (argc <= 0) {
+            abort();
+        }
+
+        if (argc != 3) {
+            usage(argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        int coordinatorPort = stoi(argv[2]);
+        if (coordinatorPort <= 0 or
+            coordinatorPort > numeric_limits<uint16_t>::max()) {
+            throw runtime_error("invalid port");
+        }
+
+        Address coordinatorAddr{argv[1], coordinatorPort};
+        ExecutionLoop loop;
+        meow::MessageParser messageParser;
+
+        shared_ptr<TCPConnection> connection =
+            loop.make_connection<TCPConnection>(
+                coordinatorAddr,
+                [&messageParser](shared_ptr<TCPConnection>, string&& data) {
+                    messageParser.parse(data);
+                    return true;
+                },
+                []() { cerr << "Error." << endl; },
+                []() { throw ProgramFinished(); });
+
+    } catch (const ProgramFinished&) {
+        return EXIT_SUCCESS;
+    } catch (const exception& e) {
+        print_exception(argv[0], e);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
--- src/execution/meow/util.hh
+++ src/execution/meow/util.hh
@@ -7,13 +7,11 @@
 
 #include "execution/connection.hh"
 #include "execution/meow/message.hh"
-#include "thunk/thunk.hh"
 
 namespace meow {
 
   std::string handle_put_message( const Message & message );
   Message create_put_message( const std::string & hash );
-  Message create_execute_message( const gg::thunk::Thunk & thunk );
 
 }
 
