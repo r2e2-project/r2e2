efa7bf06e2077eac44f8a6c352a83efa123f310e
Sadjad Fouladi <sfouladi@gmail.com>
2018-12-11 11:19:11 -0800

    Create skeleton for the Lambda function.

---
+++ src/remote/lambda-function/main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import shutil
+import subprocess as sub
+
+curdir = os.path.dirname(__file__)
+sys.path.append(curdir)
+os.environ['PATH'] = "{}:{}".format(curdir, os.environ.get('PATH', ''))
+
+def handler(event, context):
+    return {}
