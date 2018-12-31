be0df649ed9da93978b983f98293ef8a5c08ae99
Sadjad Fouladi <sfouladi@gmail.com>
2018-12-30 20:35:00 -0800

    Update lambda function handler.

--- src/remote/lambda-function/main.py
+++ src/remote/lambda-function/main.py
@@ -7,5 +7,27 @@ curdir = os.path.dirname(__file__)
 sys.path.append(curdir)
 os.environ['PATH'] = "{}:{}".format(curdir, os.environ.get('PATH', ''))
 
+def run_command(command):
+    try:
+        output = sub.check_output(command, stderr=sub.STDOUT)
+        return 0, output.decode('utf-8')
+    except sub.CalledProcessError as exc:
+        return exc.returncode, exc.output.decode('utf-8')
+
 def handler(event, context):
-    return {}
+    storage_backend = event['storageBackend']
+    coordinator_address = event['coordinator']
+    coordinator_host, coordinator_port = event['coordinator'].split(':')
+
+    # remove old worker directories
+    os.system("rm -rf /tmp/pbrt-worker.*")
+
+    retcode, output = run_command(["pbrt-lambda-worker",
+                                   coordinator_host,
+                                   coordinator_port,
+                                   storage_backend])
+
+    print(output)
+    print(retcode)
+
+    return {'returnCode': retcode, 'stdout': output}
