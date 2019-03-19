5eff022c922c16e23cdf6d34e9c08cf54fe243cf
Sadjad Fouladi <sfouladi@gmail.com>
2019-03-19 12:21:06 -0700

    Add uuid::generate.

---
+++ src/util/uuid.cpp
@@ -0,0 +1,18 @@
+#include "uuid.h"
+
+#include <uuid/uuid.h>
+
+using namespace std;
+
+namespace uuid {
+
+string generate() {
+    constexpr size_t UUID_LEN = 36;
+    char buffer[UUID_LEN + 1];
+    uuid_t uuidObj;
+    uuid_generate(uuidObj);
+    uuid_unparse_lower(uuidObj, buffer);
+    return buffer;
+}
+
+}  // namespace uuid
---
+++ src/util/uuid.h
@@ -0,0 +1,10 @@
+#ifndef PBRT_UTIL_UUID_H
+#define PBRT_UTIL_UUID_H
+
+#include <string>
+
+namespace uuid {
+std::string generate();
+}
+
+#endif /* PBRT_UTIL_UUID_H */
