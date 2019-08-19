039f05c32fb7e17eaddb1fe641b73cbb9d06939d
Sadjad Fouladi <sfouladi@gmail.com>
2019-08-19 11:07:06 -0700

    units.h: Add Mbps and kbps units.

--- src/util/units.h
+++ src/util/units.h
@@ -5,6 +5,16 @@
 
 #include <cstddef>
 
+constexpr std::size_t operator"" _kbps( unsigned long long v )
+{
+  return 1000u * v;
+}
+
+constexpr std::size_t operator"" _Mbps( unsigned long long v )
+{
+  return 1000_kbps * v;
+}
+
 constexpr std::size_t operator"" _KiB( unsigned long long v )
 {
   return 1024u * v;
