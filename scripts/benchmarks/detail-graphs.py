b564b6a18311b062ae2f49f2552d6bb7bc328214
Sadjad Fouladi <sfouladi@gmail.com>
2019-12-21 13:07:02 -0800

    detail-graphs.py: Several improvements.

--- scripts/benchmarks/detail-graphs.py
+++ scripts/benchmarks/detail-graphs.py
@@ -19,14 +19,30 @@ parser.add_argument('-o', '--out', required=True)
 
 args = parser.parse_args()
 
-COLOR_MAP = pylab.cm.get_cmap('RdYlGn', 12)
+COLOR_MAP = pylab.cm.get_cmap('Greens', 10)
+TIMESTEP = 1
+
+def get_timestep(data):
+    q = data.timestampS.unique()
+    return q[-1] - q[-2]
+
+def x_fmt(x, y):
+    global TIMESTEP
+    return '{:}'.format(int(x) * TIMESTEP)
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Y', suffix)
 
 def plot_heatmap(mat, title, xlabel, ylabel, out):
-    plt.clf()
     plt.imshow(mat.transpose(), cmap=COLOR_MAP, interpolation='nearest',
                aspect='auto', extent=(0, mat.shape[0], mat.shape[1], 0),
-               vmin=-1.5, vmax=mat.max(),
-               norm=matplotlib.colors.PowerNorm(gamma=0.2))
+               vmin=-1.5, vmax=mat.max())
+
+    plt.gca().xaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(x_fmt))
 
     plt.title(title)
     plt.xlabel(xlabel)
@@ -34,6 +50,7 @@ def plot_heatmap(mat, title, xlabel, ylabel, out):
     plt.clim(0, mat.max())
     plt.colorbar()
     plt.savefig(out, dpi=300)
+    plt.clf()
 
 def gen_per_second_per_treelet(df, out):
     per_second_per_treelet = df.groupby(['timestampS', 'treeletId']).sum()
@@ -44,7 +61,8 @@ def gen_per_second_per_treelet(df, out):
     mat = mat / timestamp_sums.reshape(-1, 1)
     mat = np.nan_to_num(mat)
 
-    plot_heatmap(mat, args.title, "Time (s)", "Treelet ID", out)
+    plot_heatmap(mat, args.title +
+                      '\nRays Dequeued (per treelet)', "Time (s)", "Treelet ID", out)
 
 def gen_per_second_per_worker(df, out):
     per_second_per_worker = df.groupby(['timestampS', 'workerId']).sum()
@@ -55,11 +73,10 @@ def gen_per_second_per_worker(df, out):
     mat = mat / timestamp_sums.reshape(-1, 1)
     mat = np.nan_to_num(mat)
 
-    plot_heatmap(mat, args.title, "Time (s)", "Worker ID", out)
+    plot_heatmap(mat, args.title +
+                      '\nRays Dequeued (per worker)', "Time (s)", "Worker ID", out)
 
 def gen_ray_queue(df, out, aggregate):
-    plt.clf()
-
     if aggregate:
         per_time = df.groupby(['timestampS']).sum()
         plt.plot((per_time.raysEnqueued - per_time.raysDequeued).cumsum())
@@ -74,7 +91,9 @@ def gen_ray_queue(df, out, aggregate):
     plt.title(args.title)
     plt.xlabel("Time (seconds)")
     plt.ylabel("Total Number of Waiting Rays")
+    plt.gcf().subplots_adjust(left=0.2)
     plt.savefig(out, dpi=300)
+    plt.clf()
 
 def sent_bytes(df, out):
     per_second_per_treelet = df.groupby(['timestampS', 'workerId']).sum()
@@ -83,7 +102,13 @@ def sent_bytes(df, out):
     mat = bytessent_ps_pt.unstack().to_numpy()
     mat = np.nan_to_num(mat)
 
-    plot_heatmap(mat, args.title, "Time (s)", "Worker ID", out)
+    max_rate = np.max(mat)
+    mat /= max_rate
+
+    plot_heatmap(mat, args.title +
+                      '\nSend Rate / Max Rate (Max = {}/s)'.format(sizeof_fmt(max_rate)),
+                 "Time (s)",
+                 "Worker ID", out)
 
 def received_bytes(df, out):
     per_second_per_treelet = df.groupby(['timestampS', 'workerId']).sum()
@@ -92,7 +117,13 @@ def received_bytes(df, out):
     mat = bytessent_ps_pt.unstack().to_numpy()
     mat = np.nan_to_num(mat)
 
-    plot_heatmap(mat, args.title, "Time (s)", "Worker ID", out)
+    max_rate = np.max(mat)
+    mat /= max_rate
+
+    plot_heatmap(mat, args.title +
+                      '\nRecv Rate / Max Rate (Max = {}/s)'.format(sizeof_fmt(max_rate)),
+                 "Time (s)",
+                 "Worker ID", out)
 
 treelet_data = pd.read_csv(os.path.join(args.input, 'treelets.csv'))
 worker_data = pd.read_csv(os.path.join(args.input, 'workers.csv'))
@@ -100,9 +131,11 @@ worker_data = pd.read_csv(os.path.join(args.input, 'workers.csv'))
 treelet_data['timestampS'] = (treelet_data.timestamp / 1000).astype('int32')
 worker_data['timestampS'] = (worker_data.timestamp / 1000).astype('int32')
 
+TIMESTEP = get_timestep(worker_data)
+
 gen_per_second_per_treelet(treelet_data, os.path.join(args.out, "per-treelet.png"))
 gen_per_second_per_worker(worker_data, os.path.join(args.out, "per-worker.png"))
 sent_bytes(worker_data, os.path.join(args.out, "per-worker-outrate.png"))
 received_bytes(worker_data, os.path.join(args.out, "per-worker-inrate.png"))
-gen_ray_queue(treelet_data, os.path.join(args.out, "aggregate-ray-queue.png"), True)
-gen_ray_queue(treelet_data, os.path.join(args.out, "individual-ray-queue.png"), False)
+gen_ray_queue(worker_data, os.path.join(args.out, "aggregate-ray-queue.png"), True)
+gen_ray_queue(worker_data, os.path.join(args.out, "individual-ray-queue.png"), False)
