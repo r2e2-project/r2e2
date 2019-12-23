a7c503bdbb9d2f70ff8cf425e372ad86df77cba2
Sadjad Fouladi <sfouladi@gmail.com>
2019-12-23 12:47:23 -0800

    nb_secure_socket: clang-format.

--- src/net/nb_secure_socket.h
+++ src/net/nb_secure_socket.h
@@ -3,82 +3,79 @@
 #ifndef PBRT_NET_NB_SECURE_SOCKET_H
 #define PBRT_NET_NB_SECURE_SOCKET_H
 
-#include <string>
 #include <deque>
+#include <string>
 
 #include "secure_socket.h"
 
-class NBSecureSocket : public SecureSocket
-{
-public:
-  enum class State { not_connected = 0,
+class NBSecureSocket : public SecureSocket {
+  public:
+    enum class State {
+        not_connected = 0,
 
-                     /* connect() */
-                     needs_connect,
-                     needs_ssl_read_to_connect,
-                     needs_ssl_write_to_connect,
+        /* connect() */
+        needs_connect,
+        needs_ssl_read_to_connect,
+        needs_ssl_write_to_connect,
 
-                     /* accept() */
-                     needs_accept,
-                     needs_ssl_read_to_accept,
-                     needs_ssl_write_to_accept,
+        /* accept() */
+        needs_accept,
+        needs_ssl_read_to_accept,
+        needs_ssl_write_to_accept,
 
-                     needs_ssl_write_to_write,
-                     needs_ssl_write_to_read,
-                     needs_ssl_read_to_write,
-                     needs_ssl_read_to_read,
-                     ready,
-                     closed };
+        needs_ssl_write_to_write,
+        needs_ssl_write_to_read,
+        needs_ssl_read_to_write,
+        needs_ssl_read_to_read,
+        ready,
+        closed
+    };
 
-  enum class Mode { not_set, connect, accept };
+    enum class Mode { not_set, connect, accept };
 
-private:
-  Mode mode_ { Mode::not_set };
-  State state_ { State::not_connected };
+  private:
+    Mode mode_{Mode::not_set};
+    State state_{State::not_connected};
 
-  std::deque<std::string> write_buffer_ {};
-  std::string read_buffer_ {};
+    std::deque<std::string> write_buffer_{};
+    std::string read_buffer_{};
 
-public:
-  NBSecureSocket( SecureSocket && sock )
-    : SecureSocket( std::move( sock ) )
-  {}
+  public:
+    NBSecureSocket(SecureSocket&& sock) : SecureSocket(std::move(sock)) {}
 
-  void connect();
-  void accept();
+    void connect();
+    void accept();
 
-  void continue_SSL_connect();
-  void continue_SSL_accept();
-  void continue_SSL_write();
-  void continue_SSL_read();
+    void continue_SSL_connect();
+    void continue_SSL_accept();
+    void continue_SSL_write();
+    void continue_SSL_read();
 
-  std::string ezread();
-  void ezwrite( const std::string & msg ) { write_buffer_.emplace_back( msg ); }
-  void ezwrite( std::string && msg ) { write_buffer_.emplace_back( move( msg ) ); }
+    std::string ezread();
+    void ezwrite(const std::string& msg) { write_buffer_.emplace_back(msg); }
+    void ezwrite(std::string&& msg) { write_buffer_.emplace_back(move(msg)); }
 
-  unsigned int buffer_bytes() const;
+    unsigned int buffer_bytes() const;
 
-  bool something_to_write() { return ( write_buffer_.size() > 0 ); }
-  bool something_to_read() { return ( read_buffer_.size() > 0 ); }
+    bool something_to_write() { return (write_buffer_.size() > 0); }
+    bool something_to_read() { return (read_buffer_.size() > 0); }
 
-  State state() const  { return state_; }
-  Mode mode() const { return mode_; }
+    State state() const { return state_; }
+    Mode mode() const { return mode_; }
 
-  bool ready() const { return state_ == State::ready; }
+    bool ready() const { return state_ == State::ready; }
 
-  bool connected() const
-  {
-    return ( state_ != State::needs_connect ) and
-           ( state_ != State::needs_ssl_read_to_connect ) and
-           ( state_ != State::needs_ssl_write_to_connect );
-  }
+    bool connected() const {
+        return (state_ != State::needs_connect) and
+               (state_ != State::needs_ssl_read_to_connect) and
+               (state_ != State::needs_ssl_write_to_connect);
+    }
 
-  bool accepted() const
-  {
-    return ( state_ != State::needs_accept ) and
-           ( state_ != State::needs_ssl_read_to_accept ) and
-           ( state_ != State::needs_ssl_write_to_accept );
-  }
+    bool accepted() const {
+        return (state_ != State::needs_accept) and
+               (state_ != State::needs_ssl_read_to_accept) and
+               (state_ != State::needs_ssl_write_to_accept);
+    }
 };
 
 #endif /* PBRT_NET_NB_SECURE_SOCKET_H */
