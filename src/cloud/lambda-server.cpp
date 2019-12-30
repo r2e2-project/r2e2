500fc4bb768172e9716b34242c7aeaa2e3d0e620
Sadjad Fouladi <sfouladi@gmail.com>
2019-12-29 16:18:12 -0800

    Create skeleton for lambda-server.

---
+++ src/cloud/lambda-server.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "execution/loop.h"
+#include "messages/utils.h"
+
+using namespace std;
+
+void usage(char const *argv0) {
+    cerr << "Usage: " << argv0 << " IP PORT" << endl;
+}
+
+int main(int argc, char const *argv[]) { return 0; }
