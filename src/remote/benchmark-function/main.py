22e3a7ffea6d98dc7c37bd6329de5c5c4f520eed
Sadjad Fouladi <sfouladi@gmail.com>
2020-01-10 08:09:54 -0800

    Create the Lambda wrapper and installer for benchmarking.

---
+++ src/remote/benchmark-function/main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import shutil
+import subprocess as sub
+
+curdir = os.path.dirname(__file__)
+sys.path.append(curdir)
+os.environ['PATH'] = "{}:{}".format(curdir, os.environ.get('PATH', ''))
+
+def handler(event, context):
+    command = ["benchmark-worker",
+               event['workerId'], event['storageUri'], event['bagSize'], event['threads'], event['duration']]
+
+    output = sub.check_output(command)
+
+    return {'output': output}
