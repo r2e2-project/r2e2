61e9830349c38312f83627e57a55b7d28fb0cee0
Sadjad Fouladi <sfouladi@gmail.com>
2020-01-19 15:56:52 -0800

    backend.cpp: Add gs:// to create_backend().

--- src/storage/backend.cpp
+++ src/storage/backend.cpp
@@ -6,6 +6,7 @@
 #include <regex>
 #include <stdexcept>
 
+#include "storage/backend_gs.h"
 #include "storage/backend_local.h"
 #include "storage/backend_s3.h"
 #include "util/digest.h"
@@ -14,26 +15,27 @@
 
 using namespace std;
 
-unique_ptr<StorageBackend> StorageBackend::create_backend( const string & uri )
-{
-  ParsedURI endpoint { uri };
+unique_ptr<StorageBackend> StorageBackend::create_backend(const string& uri) {
+    ParsedURI endpoint{uri};
 
-  unique_ptr<StorageBackend> backend;
+    unique_ptr<StorageBackend> backend;
 
-  if ( endpoint.protocol == "s3" ) {
-    backend = make_unique<S3StorageBackend>(
-      ( endpoint.username.length() or endpoint.password.length() )
-        ? AWSCredentials { endpoint.username, endpoint.password }
-        : AWSCredentials {},
-      endpoint.host,
-      endpoint.options.count( "region" )
-        ? endpoint.options[ "region" ]
-        : "us-east-1",
-      endpoint.path );
-  }
-  else {
-    throw runtime_error( "unknown storage backend" );
-  }
+    if (endpoint.protocol == "s3") {
+        backend = make_unique<S3StorageBackend>(
+            (endpoint.username.length() or endpoint.password.length())
+                ? AWSCredentials{endpoint.username, endpoint.password}
+                : AWSCredentials{},
+            endpoint.host,
+            endpoint.options.count("region") ? endpoint.options["region"]
+                                             : "us-east-1",
+            endpoint.path);
+    } else if (endpoint.protocol == "gs") {
+        backend = make_unique<GoogleStorageBackend>(
+            AWSCredentials{endpoint.username, endpoint.password}, endpoint.host,
+            endpoint.path);
+    } else {
+        throw runtime_error("unknown storage backend");
+    }
 
-  return backend;
+    return backend;
 }
