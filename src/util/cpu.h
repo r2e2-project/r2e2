0a06108b8704f12a25bdfb6756c1cd4c89f53923
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-03 16:13:35 -0800

    Create CPUStats.

---
+++ src/util/cpu.h
@@ -0,0 +1,26 @@
+#ifndef PBRT_UTIL_CPU_H
+#define PBRT_UTIL_CPU_H
+
+#include <cstdint>
+#include <chrono>
+
+class CPUStats {
+  public:
+    CPUStats();
+    CPUStats operator-(const CPUStats &other) const;
+
+    static const int64_t TIME_UNIT; /* ticks per second */
+
+    uint64_t user;
+    uint64_t nice;
+    uint64_t system;
+    uint64_t idle;
+    uint64_t iowait;
+    uint64_t irq;
+    uint64_t soft_irq;
+    uint64_t steal;
+    uint64_t guest;
+    uint64_t guest_nice;
+};
+
+#endif /* PBRT_UTIL_CPU_H */
