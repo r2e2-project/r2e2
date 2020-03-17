d53cea499a04829e3610ec1a3d7f7a2d8b47673d
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-17 07:07:51 -0700

    Import RingBuffer from mush.

---
+++ src/util/ring_buffer.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "exception.hh"
+#include "ring_buffer.hh"
+
+using namespace std;
+
+MMap_Region::MMap_Region( char* const addr,
+                          const size_t length,
+                          const int prot,
+                          const int flags,
+                          const int fd,
+                          const off_t offset )
+  : addr_( static_cast<char*>( mmap( addr, length, prot, flags, fd, offset ) ) )
+  , length_( length )
+{
+  if ( addr_ == MAP_FAILED ) {
+    throw unix_error( "mmap" );
+  }
+}
+
+MMap_Region::~MMap_Region()
+{
+  if ( addr_ ) {
+    try {
+      CheckSystemCall( "munmap", munmap( addr_, length_ ) );
+    } catch ( const exception& e ) {
+      cerr << "Exception destructing MMap_Region: " << e.what() << endl;
+    }
+  }
+}
+
+RingBuffer::RingBuffer( const size_t capacity )
+  : fd_( [&] {
+    if ( capacity % sysconf( _SC_PAGESIZE ) ) {
+      throw runtime_error( "RingBuffer capacity must be multiple of page size (" + to_string( sysconf( _SC_PAGESIZE ) )
+                           + ")" );
+    }
+    FileDescriptor fd { CheckSystemCall( "memfd_create", memfd_create( "RingBuffer", 0 ) ) };
+    CheckSystemCall( "ftruncate", ftruncate( fd.fd_num(), capacity ) );
+    return fd;
+  }() )
+  , virtual_address_space_( nullptr, 2 * capacity, PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1 )
+  , first_mapping_( virtual_address_space_.addr(),
+                    capacity,
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_FIXED,
+                    fd_.fd_num() )
+  , second_mapping_( virtual_address_space_.addr() + capacity,
+                     capacity,
+                     PROT_READ | PROT_WRITE,
+                     MAP_SHARED | MAP_FIXED,
+                     fd_.fd_num() )
+{}
+
+std::string_view RingBuffer::writable_region() const
+{
+  return { virtual_address_space_.addr() + next_index_to_write_, capacity() - bytes_stored_ };
+}
+
+simple_string_span RingBuffer::writable_region()
+{
+  return { virtual_address_space_.addr() + next_index_to_write_, capacity() - bytes_stored_ };
+}
+
+void RingBuffer::push( const size_t num_bytes )
+{
+  if ( num_bytes > writable_region().length() ) {
+    throw runtime_error( "RingBuffer::wrote exceeded size of writable region" );
+  }
+
+  next_index_to_write_ = ( next_index_to_write_ + num_bytes ) % capacity();
+  bytes_stored_ += num_bytes;
+}
+
+std::string_view RingBuffer::readable_region() const
+{
+  const size_t next_index_to_read = ( next_index_to_write_ + capacity() - bytes_stored_ ) % capacity();
+
+  return { virtual_address_space_.addr() + next_index_to_read, bytes_stored_ };
+}
+
+void RingBuffer::pop( const size_t num_bytes )
+{
+  if ( num_bytes > readable_region().length() ) {
+    throw runtime_error( "RingBuffer::pop exceeded size of readable region" );
+  }
+
+  bytes_stored_ -= num_bytes;
+}
+
+size_t RingBuffer::write( const string_view str )
+{
+  const size_t bytes_written = writable_region().copy( str );
+  push( bytes_written );
+  return bytes_written;
+}
+
+void RingBuffer::read_from( string_view& str )
+{
+  str.remove_prefix( write( str ) );
+}
---
+++ src/util/ring_buffer.hh
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <vector>
+
+#include "file_descriptor.hh"
+#include "simple_string_span.hh"
+
+class MMap_Region
+{
+  char* addr_;
+  size_t length_;
+
+public:
+  MMap_Region( char* const addr,
+               const size_t length,
+               const int prot,
+               const int flags,
+               const int fd,
+               const off_t offset = 0 );
+
+  ~MMap_Region();
+
+  /* Disallow copying */
+  MMap_Region( const MMap_Region& other ) = delete;
+  MMap_Region& operator=( const MMap_Region& other ) = delete;
+
+  char* addr() const { return addr_; }
+  size_t length() const { return length_; }
+};
+
+class RingBuffer
+{
+  size_t next_index_to_write_ = 0;
+  size_t bytes_stored_ = 0;
+
+  FileDescriptor fd_;
+  MMap_Region virtual_address_space_, first_mapping_, second_mapping_;
+
+public:
+  explicit RingBuffer( const size_t capacity );
+
+  size_t capacity() const { return first_mapping_.length(); }
+
+  simple_string_span writable_region();
+  std::string_view writable_region() const;
+  void push( const size_t num_bytes );
+
+  std::string_view readable_region() const;
+  void pop( const size_t num_bytes );
+
+  void read_from( FileDescriptor& fd ) { push( fd.read( writable_region() ) ); }
+  void write_to( FileDescriptor& fd ) { pop( fd.write( readable_region() ) ); }
+
+  size_t write( const std::string_view str );
+  void read_from( std::string_view& str );
+};
