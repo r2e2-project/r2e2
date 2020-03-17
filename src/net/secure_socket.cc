c64eef28df6f997708229f6cf69200e40d303f0c
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-17 08:36:00 -0700

    Implement TCPSession & SSLSession.

--- src/net/secure_socket.cc
+++ src/net/secure_socket.cc
@@ -1,184 +1,200 @@
-/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
-#include <cassert>
-#include <vector>
-#include <thread>
-#include <mutex>
-
 #include "secure_socket.hh"
 #include "util/exception.hh"
 
 using namespace std;
 
-class OpenSSL
+void OpenSSL::check_errors( const std::string_view context,
+                            const bool must_have_error )
 {
-private:
-    vector<mutex> locks_;
+  unsigned long first_error = ERR_get_error();
 
-    static void locking_function( int mode, int n, const char *, int )
-    {
-        if ( mode & CRYPTO_LOCK ) {
-            OpenSSL::global_context().locks_.at( n ).lock();
-        } else {
-            OpenSSL::global_context().locks_.at( n ).unlock();
-        }
+  if ( first_error == 0 ) { // no error
+    if ( must_have_error ) {
+      throw runtime_error( "SSL error, but nothing on error queue" );
+    } else {
+      return;
+    }
+  }
+
+  unsigned long next_error = ERR_get_error();
+
+  if ( next_error == 0 ) {
+    throw ssl_error( context, first_error );
+  } else {
+    string errors = ERR_error_string( first_error, nullptr );
+    errors.append( ", " );
+    errors.append( ERR_error_string( next_error, nullptr ) );
+
+    while ( unsigned long another_error = ERR_get_error() ) {
+      errors.append( ", " );
+      errors.append( ERR_error_string( another_error, nullptr ) );
     }
 
-    static unsigned long id_function( void )
-    {
-        return pthread_self();
-    }
-
-public:
-    OpenSSL()
-        : locks_( CRYPTO_num_locks() )
-    {
-        /* SSL initialization: Needs to be done exactly once */
-        /* load algorithms/ciphers */
-        SSL_library_init();
-        OpenSSL_add_all_algorithms();
-
-        /* load error messages */
-        SSL_load_error_strings();
-
-        /* set thread-safe callbacks */
-        CRYPTO_set_locking_callback( locking_function );
-        CRYPTO_set_id_callback( id_function );
-    }
-
-    static OpenSSL & global_context( void )
-    {
-        static OpenSSL os;
-        return os;
-    }
-};
-
-SSL_CTX * initialize_new_context()
-{
-    OpenSSL::global_context();
-    SSL_CTX * ret = SSL_CTX_new( SSLv23_client_method() );
-    if ( not ret ) {
-        throw ssl_error( "SSL_CTL_new" );
-    }
-    return ret;
+    throw runtime_error( "multiple SSL errors: " + errors );
+  }
 }
 
 SSLContext::SSLContext()
-    : ctx_( initialize_new_context() )
-{}
-
-SecureSocket::SecureSocket( TCPSocket && sock, SSL * ssl )
-    : TCPSocket( move( sock ) ),
-      ssl_( ssl )
+  : ctx_( SSL_CTX_new( TLS_client_method() ) )
 {
-    if ( not ssl_ ) {
-        throw runtime_error( "SecureSocket: constructor must be passed valid SSL structure" );
-    }
+  if ( not ctx_ ) {
+    OpenSSL::throw_error( "SSL_CTX_new" );
+  }
 
-    if ( not SSL_set_fd( ssl_.get(), fd_num() ) ) {
-        throw ssl_error( "SSL_set_fd" );
-    }
-
-    /* enable read/write to return only after handshake/renegotiation and successful completion */
-    SSL_set_mode( ssl_.get(), SSL_MODE_AUTO_RETRY );
-    SSL_set_mode( ssl_.get(), SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER );
+  SSL_CTX_set_mode( ctx_.get(), SSL_MODE_AUTO_RETRY );
+  SSL_CTX_set_mode( ctx_.get(), SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER );
+  SSL_CTX_set_mode( ctx_.get(), SSL_MODE_ENABLE_PARTIAL_WRITE );
+  SSL_CTX_set_verify( ctx_.get(), SSL_VERIFY_PEER, nullptr );
+  SSL_CTX_set1_cert_store( ctx_.get(), certificate_store_ );
 }
 
-SecureSocket SSLContext::new_secure_socket( TCPSocket && sock )
+void SSLContext::trust_certificate( const string_view cert_pem )
 {
-    return SecureSocket( move( sock ),
-                         SSL_new( ctx_.get() ) );
+  Certificate cert { cert_pem };
+  certificate_store_.add_certificate( cert );
 }
 
-void SecureSocket::connect( void )
+SSL_handle SSLContext::make_SSL_handle()
 {
-    ERR_clear_error();
-    int retval = SSL_connect( ssl_.get() );
-
-    if ( not retval ) {
-        throw ssl_error( "SSL_connect", SSL_get_error( ssl_.get(), retval ) );
-    }
-
-    register_read();
-    register_write();
+  SSL_handle ssl { SSL_new( ctx_.get() ) };
+  if ( not ssl ) {
+    OpenSSL::throw_error( "SSL_new" );
+  }
+  return ssl;
 }
 
-void SecureSocket::accept( const bool register_as_write )
+int tcpsocket_BIO_write( BIO* bio, const char* const buf, const int len )
 {
-    ERR_clear_error();
-    const auto ret = SSL_accept( ssl_.get() );
+  TCPSocket* sock = reinterpret_cast<TCPSocket*>( BIO_get_data( bio ) );
+  if ( not sock ) {
+    throw runtime_error( "BIO_get_data returned nullptr" );
+  }
 
-    if ( ret == 1 ) {
-        register_read();
-        return;
-    } else {
-        int error_return = SSL_get_error( ssl_.get(), ret );
+  if ( len < 0 ) {
+    throw runtime_error( "ringbuffer_BIO_write: len < 0" );
+  }
 
-        if ( error_return == SSL_ERROR_WANT_WRITE or error_return == SSL_ERROR_WANT_READ ) {
-          register_service( register_as_write );
-        }
+  const size_t bytes_written
+    = sock->write( { buf, static_cast<size_t>( len ) } );
 
-        throw ssl_error( "SSL_accept", error_return );
-    }
+  if ( bytes_written == 0 ) {
+    BIO_set_retry_write( bio );
+  }
+
+  return bytes_written;
 }
 
-string SecureSocket::read( const bool register_as_write )
+int tcpsocket_BIO_read( BIO* bio, char* const buf, const int len )
 {
-    /* SSL record max size is 16kB */
-    const size_t SSL_max_record_length = 16384;
+  TCPSocket* sock = reinterpret_cast<TCPSocket*>( BIO_get_data( bio ) );
+  if ( not sock ) {
+    throw runtime_error( "BIO_get_data returned nullptr" );
+  }
 
-    char buffer[ SSL_max_record_length ];
+  if ( len < 0 ) {
+    throw runtime_error( "ringbuffer_BIO_write: len < 0" );
+  }
 
-    ERR_clear_error();
-    ssize_t bytes_read = SSL_read( ssl_.get(), buffer, SSL_max_record_length );
+  const size_t bytes_read = sock->read( { buf, static_cast<size_t>( len ) } );
 
-    /* Make sure that we really are reading from the underlying fd */
-    assert( 0 == SSL_pending( ssl_.get() ) );
-    int error_return = SSL_get_error( ssl_.get(), bytes_read );
+  if ( bytes_read == 0 ) {
+    BIO_set_retry_read( bio );
+  }
 
-    if ( bytes_read == 0 ) {
-        if ( SSL_ERROR_ZERO_RETURN == error_return ) { /* Clean SSL close */
-            set_eof();
-        } else if ( SSL_ERROR_SYSCALL == error_return ) { /* Underlying TCP connection close */
-            /* Verify error queue is empty so we can conclude it is EOF */
-            assert( ERR_get_error() == 0 );
-            set_eof();
-        }
-        register_service( register_as_write );
-        return string(); /* EOF */
-    } else if ( bytes_read < 0 ) {
-        if ( error_return == SSL_ERROR_WANT_WRITE or error_return == SSL_ERROR_WANT_READ ) {
-          register_service( register_as_write );
-        }
-
-        throw ssl_error( "SSL_read", error_return );
-    } else {
-        /* success */
-        register_service( register_as_write );
-        return string( buffer, bytes_read );
-    }
+  return bytes_read;
 }
 
-void SecureSocket::write( const string & message, const bool register_as_read )
+long tcpsocket_BIO_ctrl( BIO*, const int cmd, const long, void* const )
 {
-    /* SSL_write returns with success if complete contents of message are written */
-    ERR_clear_error();
-    ssize_t bytes_written = SSL_write( ssl_.get(), message.data(), message.length() );
+  if ( cmd == BIO_CTRL_FLUSH ) {
+    return 1;
+  }
 
-    if ( bytes_written <= 0 ) {
-        int error_return = SSL_get_error( ssl_.get(), bytes_written );
-
-        if ( error_return == SSL_ERROR_WANT_WRITE or error_return == SSL_ERROR_WANT_READ ) {
-          register_service( not register_as_read );
-        }
-
-        throw ssl_error( "SSL_write", error_return );
-    }
-
-    register_service( not register_as_read );
+  return 0;
 }
 
-int SecureSocket::get_error( const int return_value )
+BIO_METHOD* make_method( const string& name )
 {
-    return SSL_get_error( ssl_.get(), return_value );
+  const int new_type = BIO_get_new_index();
+  if ( new_type == -1 ) {
+    OpenSSL::throw_error( "BIO_get_new_index" );
+  }
+
+  BIO_METHOD* const ret = BIO_meth_new( new_type, name.c_str() );
+  if ( not ret ) {
+    OpenSSL::throw_error( "BIO_meth_new" );
+  }
+
+  return ret;
+}
+
+TCPSocketBIO::Method::Method()
+  : method_( make_method( "TCPSocket" ) )
+{
+  if ( not BIO_meth_set_write( method_.get(), tcpsocket_BIO_write ) ) {
+    OpenSSL::throw_error( "BIO_meth_set_write" );
+  }
+
+  if ( not BIO_meth_set_read( method_.get(), tcpsocket_BIO_read ) ) {
+    OpenSSL::throw_error( "BIO_meth_set_read" );
+  }
+
+  if ( not BIO_meth_set_ctrl( method_.get(), tcpsocket_BIO_ctrl ) ) {
+    OpenSSL::throw_error( "BIO_meth_set_ctrl" );
+  }
+}
+
+const BIO_METHOD* TCPSocketBIO::Method::method()
+{
+  static Method method;
+  return method.method_.get();
+}
+
+TCPSocketBIO::TCPSocketBIO( TCPSocket&& sock )
+  : TCPSocket( move( sock ) )
+  , bio_( BIO_new( Method::method() ) )
+{
+  if ( not bio_ ) {
+    OpenSSL::throw_error( "BIO_new" );
+  }
+
+  BIO_set_data( bio_.get(), static_cast<TCPSocket*>( this ) );
+  BIO_up_ref( bio_.get() );
+  BIO_up_ref( bio_.get() );
+
+  OpenSSL::check( "TCPSocketBIO constructor" );
+}
+
+MemoryBIO::MemoryBIO( const string_view contents )
+  : contents_( move( contents ) )
+  , bio_( BIO_new_mem_buf( contents.data(), contents.size() ) )
+{
+  if ( not bio_ ) {
+    OpenSSL::throw_error( "BIO_new_mem_buf" );
+  }
+}
+
+Certificate::Certificate( const string_view contents )
+  : certificate_()
+{
+  MemoryBIO mem { contents };
+  certificate_.reset( PEM_read_bio_X509( mem, nullptr, nullptr, nullptr ) );
+  if ( not certificate_ ) {
+    OpenSSL::throw_error( "PEM_read_bio_X509" );
+  }
+}
+
+CertificateStore::CertificateStore()
+  : certificate_store_( X509_STORE_new() )
+{
+  if ( not certificate_store_ ) {
+    OpenSSL::throw_error( "X509_STORE_new" );
+  }
+}
+
+void CertificateStore::add_certificate( Certificate& cert )
+{
+  if ( not X509_STORE_add_cert( certificate_store_.get(), cert ) ) {
+    OpenSSL::throw_error( "X509_STORE_add_cert" );
+  }
 }
---
+++ src/net/session.hh
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <type_traits>
+
+#include "secure_socket.hh"
+#include "socket.hh"
+#include "util/ring_buffer.hh"
+
+template<class T, class Enable = void>
+class SessionBase;
+
+/* base for TCPSession */
+template<class T>
+class SessionBase<T, std::enable_if_t<std::is_same<T, TCPSocket>::value>>
+{
+  TCPSocket socket_;
+
+public:
+  SessionBase( TCPSocket&& socket );
+};
+
+/* base for SSLSession */
+template<class T>
+class SessionBase<T, std::enable_if_t<std::is_same<T, SSLSocket>::value>>
+{
+  SSL_handle ssl_;
+  TCPSocketBIO socket_;
+
+public:
+  SessionBase( SSL_handle&& ssl,
+               TCPSocket&& socket,
+               const std::string& hostname );
+};
+
+template<class T>
+class Session : public SessionBase<T>
+{
+private:
+  static constexpr size_t STORAGE_SIZE = 65536;
+
+  bool incoming_stream_terminated_ { false };
+
+  RingBuffer outbound_plaintext_ { STORAGE_SIZE };
+  RingBuffer inbound_plaintext_ { STORAGE_SIZE };
+
+public:
+  using SessionBase<T>::SessionBase();
+
+  TCPSocket& socket() { return socket_; }
+
+  void do_read();
+  void do_write();
+
+  bool want_read();
+  bool want_write();
+};
+
+using TCPSession = Session<TCPSocket>;
+using SSLSession = Session<SSLSocket>;
