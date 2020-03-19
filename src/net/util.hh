9eac6d10d0a13eaef9bb0aa069abfd61192fb8d7
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-18 20:02:50 -0700

    meow/message.hh: Fix a few bugs.

--- src/net/util.hh
+++ src/net/util.hh
@@ -18,7 +18,7 @@ void put_field( char* message, const uint32_t n, size_t loc );
 void put_field( char* message, const uint16_t n, size_t loc );
 
 template<class T>
-T get_field( const string_view str );
+T get_field( const std::string_view str );
 
 /* avoid implicit conversions */
 template<class T>
@@ -28,41 +28,41 @@ template<class T>
 std::string put_field( char* message, T n, size_t ) = delete;
 
 template<>
-bool get_field( const string_view str )
+bool get_field( const std::string_view str )
 {
   if ( str.length() < sizeof( bool ) ) {
-    throw std::out_of_range();
+    throw std::out_of_range( "len(str) < sizeof(bool)" );
   }
 
-  return *reinterpret_cast<const bool*>( data );
+  return *reinterpret_cast<const bool*>( str.data() );
 }
 
 template<>
-uint16_t get_field( const string_view str )
+uint16_t get_field( const std::string_view str )
 {
   if ( str.length() < sizeof( uint16_t ) ) {
-    throw std::out_of_range();
+    throw std::out_of_range( "len(str) < sizeof(uint16_t)" );
   }
 
-  return be16toh( *reinterpret_cast<const uint16_t*>( data ) );
+  return be16toh( *reinterpret_cast<const uint16_t*>( str.data() ) );
 }
 
 template<>
-uint32_t get_field( const string_view str )
+uint32_t get_field( const std::string_view str )
 {
   if ( str.length() < sizeof( uint32_t ) ) {
-    throw std::out_of_range();
+    throw std::out_of_range( "len(str) < sizeof(uint32_t)" );
   }
 
-  return be32toh( *reinterpret_cast<const uint32_t*>( data ) );
+  return be32toh( *reinterpret_cast<const uint32_t*>( str.data() ) );
 }
 
 template<>
-uint64_t get_field( const string_view str )
+uint64_t get_field( const std::string_view str )
 {
   if ( str.length() < sizeof( uint64_t ) ) {
-    throw std::out_of_range();
+    throw std::out_of_range( "len(str) < sizeof(uint64_t)" );
   }
 
-  return be64toh( *reinterpret_cast<const uint64_t*>( data ) );
+  return be64toh( *reinterpret_cast<const uint64_t*>( str.data() ) );
 }
