99b259349036e0b540a035794c25f42c69478d15
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-21 12:35:43 -0700

    status.cc: Minor updates.

--- src/master/status.cc
+++ src/master/status.cc
@@ -7,63 +7,61 @@
 using namespace std;
 using namespace std::chrono;
 using namespace r2t2;
-using namespace PollerShortNames;
 
-constexpr milliseconds EXIT_GRACE_PERIOD{5'000};
+constexpr milliseconds EXIT_GRACE_PERIOD { 5'000 };
 
-ResultType LambdaMaster::handle_status_message() {
-    ScopeTimer<TimeLog::Category::StatusBar> timer_;
+void LambdaMaster::handle_status_message()
+{
+  status_print_timer.read_event();
 
-    status_print_timer.read_event();
+  const auto now = steady_clock::now();
 
-    const auto now = steady_clock::now();
+  if ( config.timeout.count() && now - last_action_time >= config.timeout ) {
+    cerr << "Job terminated due to inactivity." << endl;
+    return;
+  } else if ( not job_timeout_timer.armed()
+              and scene.total_paths == aggregated_stats.finishedPaths ) {
+    cerr << "Done! Terminating the job in "
+         << duration_cast<seconds>( EXIT_GRACE_PERIOD ).count() << "s..."
+         << endl;
 
-    if (config.timeout.count() && now - last_action_time >= config.timeout) {
-        cerr << "Job terminated due to inactivity." << endl;
-        return ResultType::Exit;
-    } else if (jobTimeoutTimer == nullptr &&
-               scene.total_paths == aggregated_stats.finishedPaths) {
-        cerr << "Done! Terminating the job in "
-             << duration_cast<seconds>(EXIT_GRACE_PERIOD).count() << "s..."
-             << endl;
+    job_timeout_timer.set( 0s, EXIT_GRACE_PERIOD );
 
-        job_timeout_timer = make_unique<TimerFD>(EXIT_GRACE_PERIOD);
+    loop.add_rule(
+      "terminate job",
+      job_timeout_timer,
+      Direction::In,
+      [this] { terminated = true; },
+      [this] { return true; } );
+  }
 
-        loop.poller().add_action(Poller::Action(
-            *job_timeout_timer, Direction::In,
-            [this]() {
-                job_timeout_timer = nullptr;
-                return ResultType::Exit;
-            },
-            [this]() { return true; },
-            []() { throw runtime_error("job finish"); }));
-    }
+  const auto lagging_workers
+    = count_if( workers.begin(), workers.end(), [&now]( const auto& worker ) {
+        return ( worker.state != Worker::State::Terminated )
+               && ( now - worker.last_seen >= seconds { 4 } );
+      } );
 
-    const auto lagging_workers =
-        count_if(workers.begin(), workers.end(), [&now](const auto &worker) {
-            return (worker.state != Worker::State::Terminated) &&
-                   (now - worker.last_seen >= seconds{4});
-        });
+  const auto elapsed_seconds
+    = duration_cast<seconds>( now - start_time ).count();
 
-    const auto elapsed_seconds = duration_cast<seconds>(now - start_time).count();
+  auto percent = []( const uint64_t n, const uint64_t total ) -> double {
+    return total ? ( ( ( uint64_t )( 100 * ( 100.0 * n / total ) ) ) / 100.0 )
+                 : 0.0;
+  };
 
-    auto percent = [](const uint64_t n, const uint64_t total) -> double {
-        return total ? (((uint64_t)(100 * (100.0 * n / total))) / 100.0) : 0.0;
-    };
+  auto BG = []( const bool reset = false ) -> char const* {
+    constexpr char const* BG_A = "\033[48;5;022m";
+    constexpr char const* BG_B = "\033[48;5;028m";
 
-    auto BG = [](const bool reset = false) -> char const * {
-        constexpr char const *BG_A = "\033[48;5;022m";
-        constexpr char const *BG_B = "\033[48;5;028m";
+    static bool alternate = true;
+    alternate = reset ? false : !alternate;
 
-        static bool alternate = true;
-        alternate = reset ? false : !alternate;
+    return alternate ? BG_B : BG_A;
+  };
 
-        return alternate ? BG_B : BG_A;
-    };
+  auto& s = aggregated_stats;
 
-    auto &s = aggregated_stats;
-
-    // clang-format off
+  // clang-format off
     ostringstream oss;
     oss << "\033[0m" << fixed << setprecision(2)
 
@@ -72,10 +70,10 @@ ResultType LambdaMaster::handle_status_message() {
         << " (" << percent(s.finishedPaths, scene.total_paths) << "%) "
 
         << BG() << " \u21a6 " << Worker::active_count[Worker::Role::Generator]
-                << "/" << rayGenerators << " "
+                << "/" << ray_generators << " "
 
         << BG() << " \u03bb " << Worker::active_count[Worker::Role::Tracer]
-                << "/" << maxWorkers << " "
+                << "/" << max_workers << " "
 
         << BG() << " \u29d6 " << treelets_to_spawn.size() << " "
 
@@ -99,9 +97,7 @@ ResultType LambdaMaster::handle_status_message() {
                 << (elapsed_seconds % 60) << " "
 
         << BG();
-    // clang-format on
+  // clang-format on
 
-    StatusBar::set_text(oss.str());
-
-    return ResultType::Continue;
+  StatusBar::set_text( oss.str() );
 }
