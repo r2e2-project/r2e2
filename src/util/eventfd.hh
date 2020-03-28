e069c2849eb506f5cdfd623a8162cd82349473e6
Sadjad Fouladi <sfouladi@gmail.com>
2020-03-27 21:23:24 -0700

    eventfd.hh: Register read/write in {read,write}_event().

--- src/util/eventfd.hh
+++ src/util/eventfd.hh
@@ -10,29 +10,37 @@
 #include "util/exception.hh"
 #include "util/file_descriptor.hh"
 
-class EventFD : public FileDescriptor {
-  public:
-    EventFD(const bool semaphore = false)
-        : FileDescriptor(
-              eventfd(0u, (semaphore ? EFD_SEMAPHORE : 0) | EFD_NONBLOCK)) {}
+class EventFD : public FileDescriptor
+{
+public:
+  EventFD( const bool semaphore = false )
+    : FileDescriptor(
+      eventfd( 0u, ( semaphore ? EFD_SEMAPHORE : 0 ) | EFD_NONBLOCK ) )
+  {}
 
-    bool read_event() {
-        uint64_t value;
-        int retval = ::read(fd_num(), &value, sizeof(value));
+  bool read_event()
+  {
+    uint64_t value;
+    int retval = ::read( fd_num(), &value, sizeof( value ) );
 
-        if (retval == sizeof(value)) {
-            return true;
-        } else if (retval < 0 && errno == EAGAIN) {
-            return false;
-        } else {
-            throw unix_error("eventfd_read");
-        }
+    register_read();
+
+    if ( retval == sizeof( value ) ) {
+      return true;
+    } else if ( retval < 0 && errno == EAGAIN ) {
+      return false;
+    } else {
+      throw unix_error( "eventfd_read" );
+    }
+  }
+
+  void write_event()
+  {
+    uint64_t value = 1;
+    if ( ::write( fd_num(), &value, sizeof( value ) ) < 0 ) {
+      throw unix_error( "eventfd_write" );
     }
 
-    void write_event() {
-        uint64_t value = 1;
-        if (::write(fd_num(), &value, sizeof(value)) < 0) {
-            throw unix_error("eventfd_write");
-        }
-    }
+    register_write();
+  }
 };
