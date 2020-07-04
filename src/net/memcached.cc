3aacea7f884b9a2e8a37c011184421cccdfa5b31
Sadjad Fouladi <sfouladi@gmail.com>
2020-07-04 11:20:14 -0700

    memcached.hh: Clean up.

--- src/net/memcached.cc
+++ src/net/memcached.cc
@@ -4,11 +4,118 @@ using namespace std;
 
 namespace memcached {
 
+size_t ResponseParser::parse( const string_view data )
+{
+  auto startswith = []( const string& token, const char* cstr ) -> bool {
+    return ( token.compare( 0, strlen( cstr ), cstr ) == 0 );
+  };
+
+  const size_t input_length = data.length();
+  raw_buffer_.append( data );
+
+  bool must_continue = true;
+
+  while ( must_continue ) {
+    if ( raw_buffer_.empty() )
+      break;
+
+    switch ( state_ ) {
+      case State::FirstLinePending: {
+        const auto crlf_index = raw_buffer_.find( CRLF );
+        if ( crlf_index == string::npos ) {
+          must_continue = false;
+          break;
+        }
+
+        response_.first_line_ = raw_buffer_.substr( 0, crlf_index );
+        response_.unstructured_data_ = "";
+
+        raw_buffer_.erase( 0, crlf_index + 2 );
+
+        const auto first_space = response_.first_line_.find( ' ' );
+        const auto first_word = response_.first_line_.substr( 0, first_space );
+
+        if ( first_word == "VALUE" ) {
+          response_.type_ = Response::Type::VALUE;
+
+          const auto last_space = response_.first_line_.rfind( ' ' );
+          const size_t length
+            = stoull( response_.first_line_.substr( last_space + 1 ) );
+
+          state_ = State::BodyPending;
+          expected_body_length_ = length;
+        } else {
+          if ( first_word == "STORED" ) {
+            response_.type_ = Response::Type::STORED;
+          } else if ( first_word == "NOT_STORED" ) {
+            response_.type_ = Response::Type::NOT_STORED;
+          } else if ( first_word == "DELETED" ) {
+            response_.type_ = Response::Type::DELETED;
+          } else if ( first_word == "ERROR" ) {
+            response_.type_ = Response::Type::ERROR;
+          } else if ( first_word == "NOT_FOUND" ) {
+            response_.type_ = Response::Type::NOT_FOUND;
+          } else if ( first_word == "OK" ) {
+            response_.type_ = Response::Type::OK;
+          } else if ( first_word == "END"
+                      && requests_.front() == Request::Type::GET ) {
+            response_.type_ = Response::Type::NOT_FOUND;
+          } else {
+            throw runtime_error(
+              "invalid response: " + response_.first_line_ + " (request: "
+              + to_string( static_cast<int>( requests_.front() ) ) + ")" );
+          }
+
+          requests_.pop();
+          responses_.push( move( response_ ) );
+
+          state_ = State::FirstLinePending;
+          expected_body_length_ = 0;
+        }
+
+        break;
+      }
+      case State::BodyPending: {
+        if ( raw_buffer_.length() >= expected_body_length_ + 2 ) {
+          response_.unstructured_data_
+            = raw_buffer_.substr( 0, expected_body_length_ );
+
+          raw_buffer_.erase( 0, expected_body_length_ + 2 );
+
+          state_ = State::LastLinePending;
+          expected_body_length_ = 0;
+        } else {
+          must_continue = false;
+        }
+
+        break;
+      }
+
+      case State::LastLinePending: {
+        if ( startswith( raw_buffer_, "END\r\n" ) ) {
+          requests_.pop();
+          responses_.push( move( response_ ) );
+
+          state_ = State::FirstLinePending;
+          expected_body_length_ = 0;
+          raw_buffer_.erase( 0, strlen( "END\r\n" ) );
+        } else {
+          must_continue = false;
+        }
+
+        break;
+      }
+    }
+  }
+
+  return input_length;
+}
+
 void Client::load()
 {
   if ( ( not current_request_first_line_.empty() )
        or ( not current_request_data_.empty() ) or ( requests_.empty() ) ) {
-    throw std::runtime_error( "memcached::Client cannot load a new request" );
+    throw runtime_error( "memcached::Client cannot load a new request" );
   }
 
   current_request_first_line_ = requests_.front().first_line();
@@ -39,7 +146,7 @@ void Client::read( RingBuffer& in )
 void Client::write( RingBuffer& out )
 {
   if ( requests_empty() ) {
-    throw std::runtime_error( "Client::write(): Client has no more requests" );
+    throw runtime_error( "Client::write(): Client has no more requests" );
   }
 
   if ( not current_request_first_line_.empty() ) {
--- src/net/memcached.hh
+++ src/net/memcached.hh
@@ -46,40 +46,6 @@ public:
   }
 };
 
-class SetRequest : public Request
-{
-public:
-  SetRequest( const std::string& key, const std::string& data )
-    : Request( Request::Type::SET,
-               "set " + key + " 0 0 " + std::to_string( data.length() ),
-               data )
-  {}
-};
-
-class GetRequest : public Request
-{
-public:
-  GetRequest( const std::string& key )
-    : Request( Request::Type::GET, "get " + key, "" )
-  {}
-};
-
-class DeleteRequest : public Request
-{
-public:
-  DeleteRequest( const std::string& key )
-    : Request( Request::Type::DELETE, "delete " + key, "" )
-  {}
-};
-
-class FlushRequest : public Request
-{
-public:
-  FlushRequest()
-    : Request( Request::Type::FLUSH, "flush_all", "" )
-  {}
-};
-
 class Response
 {
 public:
@@ -133,124 +99,46 @@ private:
   Response response_ {};
 
 public:
-  template<class T>
-  void new_request( const T& req )
-  {
-    requests_.push( req.type() );
-  }
+  void new_request( const Request& req ) { requests_.push( req.type() ); }
 
-  size_t parse( const std::string_view data )
-  {
-    auto startswith = []( const std::string& token, const char* cstr ) -> bool {
-      return ( token.compare( 0, strlen( cstr ), cstr ) == 0 );
-    };
-
-    const size_t input_length = data.length();
-    raw_buffer_.append( data );
-
-    bool must_continue = true;
-
-    while ( must_continue ) {
-      if ( raw_buffer_.empty() )
-        break;
-
-      switch ( state_ ) {
-        case State::FirstLinePending: {
-          const auto crlf_index = raw_buffer_.find( CRLF );
-          if ( crlf_index == std::string::npos ) {
-            must_continue = false;
-            break;
-          }
-
-          response_.first_line_ = raw_buffer_.substr( 0, crlf_index );
-          response_.unstructured_data_ = "";
-
-          raw_buffer_.erase( 0, crlf_index + 2 );
-
-          const auto first_space = response_.first_line_.find( ' ' );
-          const auto first_word
-            = response_.first_line_.substr( 0, first_space );
-
-          if ( first_word == "VALUE" ) {
-            response_.type_ = Response::Type::VALUE;
-
-            const auto last_space = response_.first_line_.rfind( ' ' );
-            const size_t length
-              = stoull( response_.first_line_.substr( last_space + 1 ) );
-
-            state_ = State::BodyPending;
-            expected_body_length_ = length;
-          } else {
-            if ( first_word == "STORED" ) {
-              response_.type_ = Response::Type::STORED;
-            } else if ( first_word == "NOT_STORED" ) {
-              response_.type_ = Response::Type::NOT_STORED;
-            } else if ( first_word == "DELETED" ) {
-              response_.type_ = Response::Type::DELETED;
-            } else if ( first_word == "ERROR" ) {
-              response_.type_ = Response::Type::ERROR;
-            } else if ( first_word == "NOT_FOUND" ) {
-              response_.type_ = Response::Type::NOT_FOUND;
-            } else if ( first_word == "OK" ) {
-              response_.type_ = Response::Type::OK;
-            } else if ( first_word == "END"
-                        && requests_.front() == Request::Type::GET ) {
-              response_.type_ = Response::Type::NOT_FOUND;
-            } else {
-              throw std::runtime_error(
-                "invalid response: " + response_.first_line_ + " (request: "
-                + std::to_string( static_cast<int>( requests_.front() ) )
-                + ")" );
-            }
-
-            requests_.pop();
-            responses_.push( std::move( response_ ) );
-
-            state_ = State::FirstLinePending;
-            expected_body_length_ = 0;
-          }
-
-          break;
-        }
-        case State::BodyPending: {
-          if ( raw_buffer_.length() >= expected_body_length_ + 2 ) {
-            response_.unstructured_data_
-              = raw_buffer_.substr( 0, expected_body_length_ );
-
-            raw_buffer_.erase( 0, expected_body_length_ + 2 );
-
-            state_ = State::LastLinePending;
-            expected_body_length_ = 0;
-          } else {
-            must_continue = false;
-          }
-
-          break;
-        }
-
-        case State::LastLinePending: {
-          if ( startswith( raw_buffer_, "END\r\n" ) ) {
-            requests_.pop();
-            responses_.push( std::move( response_ ) );
-
-            state_ = State::FirstLinePending;
-            expected_body_length_ = 0;
-            raw_buffer_.erase( 0, strlen( "END\r\n" ) );
-          } else {
-            must_continue = false;
-          }
-
-          break;
-        }
-      }
-    }
-
-    return input_length;
-  }
-
+  size_t parse( const std::string_view data );
   bool empty() const { return responses_.empty(); }
   Response& front() { return responses_.front(); }
   void pop() { responses_.pop(); }
+};
+
+class SetRequest : public Request
+{
+public:
+  SetRequest( const std::string& key, const std::string& data )
+    : Request( Request::Type::SET,
+               "set " + key + " 0 0 " + std::to_string( data.length() ),
+               data )
+  {}
+};
+
+class GetRequest : public Request
+{
+public:
+  GetRequest( const std::string& key )
+    : Request( Request::Type::GET, "get " + key, "" )
+  {}
+};
+
+class DeleteRequest : public Request
+{
+public:
+  DeleteRequest( const std::string& key )
+    : Request( Request::Type::DELETE, "delete " + key, "" )
+  {}
+};
+
+class FlushRequest : public Request
+{
+public:
+  FlushRequest()
+    : Request( Request::Type::FLUSH, "flush_all", "" )
+  {}
 };
 
 class Client : public ::Client<TCPSession, Request, Response>
