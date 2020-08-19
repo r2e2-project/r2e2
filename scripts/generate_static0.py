06fb2a5c8245495fea8b775332df083fa1223aa9
Sadjad Fouladi <sfouladi@gmail.com>
2020-08-18 19:09:33 -0700

    generate_static0.py: Minor adjustment of parameters.

--- scripts/generate_static0.py
+++ scripts/generate_static0.py
@@ -24,11 +24,11 @@ def get_data(path):
     return num_rays, nodes_per_ray
 
 def generate_static0(num_rays, nodes_per_ray):
-    average_ray_size = 500         # 500 bytes
-    average_bandwidth = 25_000_000 # 25 MB/s receive
+    average_ray_size = 250         # 500 bytes
+    average_bandwidth = 50_000_000 # 25 MB/s receive
 
     cost_bw = average_ray_size / average_bandwidth
-    cost_compute = 1e-6 # 1us per node visited
+    cost_compute = 0.25e-6 # 0.5us per node visited
     
     weights = np.maximum(cost_bw * num_rays,
                          cost_compute * nodes_per_ray * num_rays)
