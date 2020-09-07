372e30afb720a7f5dc7433d86ce3576d790b0e07
Sadjad Fouladi <sfouladi@gmail.com>
2020-09-06 18:18:29 -0700

    lambda-worker.hh: Set the base bagging delay to 50ms.

--- src/common/lambda.hh
+++ src/common/lambda.hh
@@ -9,7 +9,7 @@
 
 #include "util/uri.hh"
 
-constexpr std::chrono::milliseconds DEFAULT_BAGGING_DELAY { 100 };
+constexpr std::chrono::milliseconds DEFAULT_BAGGING_DELAY { 50 };
 constexpr size_t WORKER_MAX_ACTIVE_RAYS = 100'000; /* ~120 MiB of rays */
 
 using WorkerId = uint64_t;
