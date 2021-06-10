6c001a23ab3269ea2cfe95fa4ae1373c5dc93e63
Sadjad Fouladi <sfouladi@gmail.com>
2021-06-09 22:25:12 -0700

    WIP: move accumulator helpers to common/ under a new name.

---
+++ src/common/tile_helper.cc
@@ -0,0 +1,62 @@
+#include "tile_helper.hh"
+
+#include <algorithm>
+#include <cmath>
+#include <memory>
+
+using namespace std;
+
+namespace r2t2 {
+
+TileHelper::TileHelper( const uint32_t accumulators,
+                        const pbrt::Bounds2i& sample_bounds,
+                        const uint32_t spp )
+  : accumulators_( accumulators )
+  , bounds_( sample_bounds )
+  , extent_( bounds_.Diagonal() )
+  , spp_( spp )
+{
+  tile_size_ = ceil( sqrt( extent_.x * extent_.y / accumulators_ ) );
+
+  while ( ceil( 1.0 * extent_.x / tile_size_ )
+            * ceil( 1.0 * extent_.y / tile_size_ )
+          > accumulators_ ) {
+    tile_size_++;
+  }
+
+  active_accumulators_
+    = static_cast<uint32_t>( ceil( 1.0 * extent_.x / tile_size_ )
+                             * ceil( 1.0 * extent_.y / tile_size_ ) );
+
+  n_tiles_ = { ( extent_.x + tile_size_ - 1 ) / tile_size_,
+               ( extent_.y + tile_size_ - 1 ) / tile_size_ };
+}
+
+TileId TileHelper::tile_id( const pbrt::Sample& sample ) const
+{
+  if ( active_accumulators_ <= 1 )
+    return 0;
+
+  const auto pixel = sample.SamplePixel(
+    { static_cast<int>( extent_.x ), static_cast<int>( extent_.y ) }, spp_ );
+  return pixel.x / tile_size_ + pixel.y / tile_size_ * n_tiles_.x;
+}
+
+pbrt::Bounds2<uint32_t> TileHelper::bounds( const uint32_t tile_id ) const
+{
+  if ( tile_id >= active_accumulators_ ) {
+    throw runtime_error( "empty bounds" );
+  }
+
+  const uint32_t tile_x = tile_id % n_tiles_.x;
+  const uint32_t tile_y = tile_id / n_tiles_.x;
+
+  const uint32_t x0 = bounds_.pMin.x + tile_x * tile_size_;
+  const uint32_t x1 = min( x0 + tile_size_, bounds_.pMax.x );
+  const uint32_t y0 = bounds_.pMin.y + tile_y * tile_size_;
+  const uint32_t y1 = min( y0 + tile_size_, bounds_.pMax.y );
+
+  return { { x0, y0 }, { x1, y1 } };
+}
+
+}
---
+++ src/common/tile_helper.hh
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <pbrt/main.h>
+#include <pbrt/raystate.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "lambda.hh"
+
+#include "messages/message.hh"
+#include "net/address.hh"
+#include "net/client.hh"
+#include "net/session.hh"
+#include "net/socket.hh"
+#include "net/transfer_s3.hh"
+#include "util/eventloop.hh"
+#include "util/exception.hh"
+#include "util/temp_dir.hh"
+#include "util/timerfd.hh"
+
+#include "concurrentqueue/blockingconcurrentqueue.h"
+
+namespace r2t2 {
+
+class TileHelper
+{
+private:
+  uint32_t accumulators_ { 0 };
+  pbrt::Bounds2<uint32_t> bounds_ {};
+  pbrt::Vector2<uint32_t> extent_ {};
+  uint32_t spp_ {};
+
+  uint32_t tile_size_ {};
+  uint32_t active_accumulators_ {};
+
+  pbrt::Vector2<uint32_t> n_tiles_ { 1, 1 };
+
+public:
+  TileHelper() = default;
+  TileHelper( const TileHelper& ) = default;
+  TileHelper& operator=( const TileHelper& ) = default;
+
+  TileHelper( const uint32_t accumulators,
+              const pbrt::Bounds2i& sample_bounds,
+              const uint32_t spp );
+
+  TileId tile_id( const pbrt::Sample& sample ) const;
+
+  uint32_t tile_size() const { return tile_size_; }
+  uint32_t active_accumulators() const { return active_accumulators_; }
+
+  pbrt::Bounds2<uint32_t> bounds( const uint32_t tile_id ) const;
+};
+
+}
